ceef9dbd93c4942549f44d69c082460ee25372fd
Maximilian Gerhardt <maxi.gerhardt@googlemail.com>
2018-09-26 21:33:29 +0200

    Rename toolchain file, add comment on exporting env-vars

--- omega_toolchain.cmake
+++ omega_toolchain.cmake
@@ -1,13 +1,22 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
+# Tell CMake about the to-be-used C/C++ compiler.
+
 set(CMAKE_SYSROOT /home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16)
-#set(CMAKE_STAGING_PREFIX /home/devel/stage)
+
+# I think I added
+#  PATH="/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16/bin:$PATH"
+#
+#  STAGING_DIR="/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16"
+#  export STAGING_DIR
+#
+#  LEDE_INCLUDE="/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16/include/"
+#  export LEDE_INCLUDE
+# To my /etc/bash.bashrc to finally set the STAGING_DIR variable successfully. Or the ~/.bashrc?
 
 set(ENV{STAGING_DIR} "/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16")
 set(CMAKE_C_COMPILER "/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16/bin/mipsel-openwrt-linux-gcc")
 set(CMAKE_CXX_COMPILER "/home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16/bin/mipsel-openwrt-linux-g++")
-
-#home/max/source/staging_dir/toolchain-mipsel_24kc_gcc-5.4.0_musl-1.1.16
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
